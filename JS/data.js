16bf2932f77cc71b9fc04dae67669a6d25538000
aviya matzliach <aviya1103@gmail.com>
2026-04-05 17:47:07 +0300

    define persons and questions arrays in data+JSDoc

---
+++ JS/data.js
@@ -0,0 +1,384 @@
+/**
+ * @typedef {Object} Person  
+ * @property {string} img 
+ * @property {string} Min 
+ * @property {string} hairColor
+ * @property {string} glasses 
+ * @property {string} hat
+ * @property {string} clothesColor
+ * @property {string} skinColor
+ */
+
+export const questionsType = [
+    { title: 'מין', key: 'Min', questions: ['איש', 'אישה'] },
+    { title: 'צבע שיער', key: 'hairColor', questions: ['בלונד','חום','שחור']},
+    { title: 'שפם', key: 'Safam', questions: ['כן', 'לא']},
+    { title: 'משקפיים',key: 'glasses', questions: ['כן', 'לא']},
+    { title: 'כובע', key: 'hat', questions: ['כן', 'לא']},
+    { title: 'צבע בגד', key: 'clothesColor', questions: ['אדום', 'כתום', 'ירוק', 'לבן', 'שחור', 'ורוד']},
+    { title: 'צבע עור', key: 'skinColor', questions: ['בהיר', 'כהה']}
+];
+
+export const persons = [
+    {
+        img: '../IMAGES/001-man.svg', 
+        Min: 'איש',
+        hairColor: 'שחור',
+        Safam: 'לא',
+        glasses: 'לא',
+        hat: 'לא',
+        clothesColor: 'אדום',
+        skinColor: 'בהיר'
+    },
+        {
+        img: '../IMAGES/002-man.svg',
+        Min: 'איש',
+        hairColor: 'בלונד',
+        Safam: 'לא',
+        glasses: 'לא',
+        hat: 'לא',
+        clothesColor: 'כתום',
+        skinColor: 'בהיר'
+    },
+    {
+        img: '../IMAGES/003-man.svg',
+        Min: 'איש',
+        hairColor: 'שחור',
+        Safam: 'לא',
+        glasses: 'לא',
+        hat: 'לא',
+        clothesColor: 'ירוק',
+        skinColor: 'בהיר'
+    }, 
+    {
+        img: '../IMAGES/004-man.svg',
+        Min: 'איש',
+        hairColor: 'בלונד',
+        Safam: 'לא',
+        glasses: 'לא',
+        hat: 'כן',
+        clothesColor: 'אדום',
+        skinColor: 'בהיר'
+    }, 
+    {
+        img: '../IMAGES/005-man.svg',
+        Min: 'איש',
+        hairColor: 'שחור',
+        Safam: 'לא',
+        glasses: 'לא',
+        hat: 'לא',
+        clothesColor: 'לבן',
+        skinColor: 'בהיר'
+    }, 
+    {
+        img: '../IMAGES/006-man.svg',
+        Min: 'איש',
+        hairColor: 'חום',
+        Safam: 'כן',
+        glasses: 'לא',
+        hat: 'לא',
+        clothesColor: 'ירוק',
+        skinColor: 'בהיר'
+    
+    }, 
+    {
+        img: '../IMAGES/007-woman.svg',
+        Min: 'אישה',
+        hairColor: 'שחור',
+        Safam: 'לא',
+        glasses: 'לא',
+        hat: 'לא',
+        clothesColor: 'ורוד',
+        skinColor: 'בהיר'
+    }, 
+    {
+        img: '../IMAGES/008-woman.svg',
+        Min: 'אישה',
+        hairColor: 'שחור',
+        Safam: 'לא',
+        glasses: 'לא',
+        hat: 'לא',
+        clothesColor: 'אדום',
+        skinColor: 'בהיר'
+    }, 
+    {
+        img: '../IMAGES/009-woman.svg',
+        Min: 'אישה',
+        hairColor: 'שחור',
+        Safam: 'לא',
+        glasses: 'לא',
+        hat: 'לא',
+        clothesColor: 'לבן',
+        skinColor: 'בהיר'
+    }, 
+    {
+        img: '../IMAGES/010-woman.svg',
+        Min: 'אישה',
+        hairColor: 'חום',
+        Safam: 'לא',
+        glasses: 'לא',
+        hat: 'לא',
+        clothesColor: 'ורוד',
+        skinColor: 'בהיר'
+    }, 
+    {
+        img: '../IMAGES/011-woman.svg',
+        Min: 'אישה',
+        hairColor: 'שחור',
+        Safam: 'לא',
+        glasses: 'לא',
+        hat: 'לא',
+        clothesColor: 'כתום',
+        skinColor: 'בהיר'
+    }, 
+    {
+        img: '../IMAGES/012-woman.svg',
+        Min: 'אישה',
+        hairColor: 'שחור',
+        Safam: 'לא',
+        glasses: 'לא',
+        hat: 'לא',
+        clothesColor: 'ורוד',
+        skinColor: 'בהיר'
+    }, 
+    {
+        img: '../IMAGES/013-woman.svg',
+        Min: 'אישה',
+        hairColor: 'שחור',
+        Safam: 'לא',
+        glasses: 'לא',
+        hat: 'לא',
+        clothesColor: 'שחור',
+        skinColor: 'בהיר'
+    }, 
+    {
+        img: '../IMAGES/014-woman.svg',
+        Min: 'אישה',
+        hairColor: 'שחור',
+        Safam: 'לא',
+        glasses: 'לא',
+        hat: 'לא',
+        clothesColor: 'לבן',
+        skinColor: 'בהיר'
+    }, 
+    {
+        img: '../IMAGES/015-woman.svg',
+        Min: 'אישה',
+        hairColor: 'חום',
+        Safam: 'לא',
+        glasses: 'כן',
+        hat: 'לא',
+        clothesColor: 'כתום',
+        skinColor: 'בהיר'
+    }, 
+    {
+        img: '../IMAGES/016-woman.svg',
+        Min: 'אישה',
+        hairColor: 'שחור',
+        Safam: 'לא',
+        glasses: 'לא',
+        hat: 'כן',
+        clothesColor: 'ורוד',
+        skinColor: 'בהיר'
+    }, 
+    {
+        img: '../IMAGES/017-man.svg',
+        Min: 'איש',
+        hairColor: 'בלונד',
+        Safam: 'לא',
+        glasses: 'לא',
+        hat: 'לא',
+        clothesColor: 'שחור',
+        skinColor: 'בהיר'
+    },
+    {
+        img: '../IMAGES/018-man.svg',
+        Min: 'איש',
+        hairColor: 'שחור',
+        Safam: 'לא',
+        glasses: 'לא',
+        hat: 'לא',
+        clothesColor: 'לבן',
+        skinColor: 'כהה'
+    }, 
+    {
+        img: '../IMAGES/019-man.svg',
+        Min: 'איש',
+        hairColor: 'בלונד',
+        Safam: 'לא',
+        glasses: 'לא',
+        hat: 'לא',
+        clothesColor: 'כתום',
+        skinColor: 'בהיר'
+    }, 
+    {
+        img: '../IMAGES/020-man.svg',
+        Min: 'איש',
+        hairColor: 'בלונד',
+        Safam: 'לא',
+        glasses: 'לא',
+        hat: 'לא',
+        clothesColor: 'שחור',
+        skinColor: 'כהה'
+    }, 
+    {
+        img: '../IMAGES/021-man.svg',
+        Min: 'איש',
+        hairColor: 'שחור',
+        Safam: 'כן',
+        glasses: 'לא',
+        hat: 'לא',
+        clothesColor: 'כתום',
+        skinColor: 'כהה'
+    }, 
+    {
+        img: '../IMAGES/022-man.svg',
+        Min: 'איש',
+        hairColor: 'שחור',
+        Safam: 'לא',
+        glasses: 'כן',
+        hat: 'לא',
+        clothesColor: 'שחור',
+        skinColor: 'כהה'
+    },
+     {
+        img: '../IMAGES/023-man.svg',
+        Min: 'איש',
+        hairColor: 'שחור',
+        Safam: 'לא',
+        glasses: 'לא',
+        hat: 'לא',
+        clothesColor: 'כתום',
+        skinColor: 'כהה'
+    }, 
+    {
+        img: '../IMAGES/024-man.svg',
+        Min: 'איש',
+        hairColor: 'שחור',
+        Safam: 'לא',
+        glasses: 'לא',
+        hat: 'לא',
+        clothesColor: 'אדום',
+        skinColor: 'בהיר'
+    }, 
+    {
+        img: '../IMAGES/025-man.svg',
+        Min: 'איש',
+        hairColor: 'שחור',
+        Safam: 'לא',
+        glasses: 'לא',
+        hat: 'לא',
+        clothesColor: 'אדום',
+        skinColor: 'כהה'
+    }, 
+    {
+        img: '../IMAGES/026-man.svg',
+        Min: 'איש',
+        hairColor: 'שחור',
+        Safam: 'לא',
+        glasses: 'לא',
+        hat: 'לא',
+        clothesColor: 'אדום',
+        skinColor: 'כהה'
+    }, 
+    {
+        img: '../IMAGES/027-woman.svg',
+        Min: 'אישה',
+        hairColor: 'שחור',
+        Safam: 'לא',
+        glasses: 'לא',
+        hat: 'לא',
+        clothesColor: 'כתום',
+        skinColor: 'כהה'
+    }, 
+    {
+        img: '../IMAGES/028-woman.svg',
+        Min: 'אישה',
+        hairColor: 'שחור',
+        Safam: 'לא',
+        glasses: 'לא',
+        hat: 'לא',
+        clothesColor: 'ורוד',
+        skinColor: 'כהה'
+    }, 
+    {
+        img: '../IMAGES/029-woman.svg',
+        Min: 'אישה',
+        hairColor: 'שחור',
+        Safam: 'לא',
+        glasses: 'לא',
+        hat: 'כן',
+        clothesColor: 'ורוד',
+        skinColor: 'כהה'
+    }, 
+    {
+        img: '../IMAGES/030-woman.svg',
+        Min: 'אישה',
+        hairColor: 'בלונד',
+        Safam: 'לא',
+        glasses: 'לא',
+        hat: 'לא',
+        clothesColor: 'כתום',
+        skinColor: 'כהה'
+    }, 
+    {
+        img: '../IMAGES/031-woman.svg',
+        Min: 'אישה',
+        hairColor: 'שחור',
+        Safam: 'לא',
+        glasses: 'לא',
+        hat: 'לא',
+        clothesColor: 'ירוק',
+        skinColor: 'כהה'
+    }, 
+    {
+        img: '../IMAGES/032-woman.svg',
+        Min: 'אישה',
+        hairColor: 'שחור',
+        Safam: 'לא',
+        glasses: 'כן',
+        hat: 'לא',
+        clothesColor: 'ורוד',
+        skinColor: 'כהה'
+    }, 
+    {
+        img: '../IMAGES/033-man.svg',
+        Min: 'איש',
+        hairColor: 'שחור',
+        Safam: 'כן',
+        glasses: 'לא',
+        hat: 'לא',
+        clothesColor: 'ירוק',
+        skinColor: 'בהיר'
+    }, 
+    {
+        img: '../IMAGES/034-man.svg',
+        Min: 'איש',
+        hairColor: 'שחור',
+        Safam: 'לא',
+        glasses: 'לא',
+        hat: 'לא',
+        clothesColor: 'אדום',
+        skinColor: 'בהיר'
+    }, 
+    {
+        img: '../IMAGES/035-man.svg',
+        Min: 'איש',
+        hairColor: 'שחור',
+        Safam: 'לא',
+        glasses: 'לא',
+        hat: 'לא',
+        clothesColor: 'שחור',
+        skinColor: 'בהיר'
+    }, 
+    {
+        img: '../IMAGES/036-man.svg',
+        Min: 'איש',
+        hairColor: 'בלונד',
+        Safam: 'לא',
+        glasses: 'לא',
+        hat: 'לא',
+        clothesColor: 'שחור',
+        skinColor: 'בהיר'
+    }
+];
